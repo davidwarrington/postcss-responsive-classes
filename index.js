1c1c093d714ce582b8bc7368ec9d7639ab4386fb
David Warrington <davidmwarrington+github@gmail.com>
2022-04-10 22:44:26 +0100

    feat(plugin): use all custom-media definitions found in stylesheet

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
  * @type {import('postcss').PluginCreator}
  */
 module.exports = () => {
+  const customMediaNames = [];
+
   return {
     postcssPlugin: 'postcss-responsive-classes',
 
     AtRule: {
       responsive: (rule, { AtRule }) => {
-        const responsiveVariants = ['sm'].flatMap(variant => {
+        const responsiveVariants = customMediaNames.flatMap(variant => {
           const wrapper = new AtRule({
             name: 'media',
             params: `(--${variant})`,
@@ -25,6 +27,19 @@ module.exports = () => {
 
         rule.replaceWith(...rule.nodes, ...responsiveVariants);
       },
+    },
+
+    Once(root) {
+      const customMediaRules = root.nodes.filter(
+        node => node.type === 'atrule' && node.name === 'custom-media'
+      );
+
+      customMediaRules.forEach(rule => {
+        const [name] = rule.params.split(' ');
+        const unprefixedName = name.replace(/^--/, '');
+
+        customMediaNames.push(unprefixedName);
+      });
     },
   };
 };
--- index.test.js
+++ index.test.js
@@ -12,30 +12,48 @@ async function run(input, output, options = {}) {
 
 it('replaces at-rule with media-query alternatives', async () => {
   await run(
-    `@responsive { .bg-tomato { background: tomato; } }`,
+    `@custom-media --sm (min-width: 768px); @responsive { .bg-tomato { background: tomato; } }`,
     [
+      `@custom-media --sm (min-width: 768px);`,
       `.bg-tomato { background: tomato; }`,
-      `@media (--sm) { .bg-tomato\\@sm { background: tomato; } }`,
+      `@media (--sm){ .bg-tomato\\@sm { background: tomato; } }`,
     ].join(' ')
   );
 });
 
 it('supports multiple child nodes per at-rule', async () => {
   await run(
-    `@responsive { .bg-tomato { background: tomato; } .bg-coral { background: coral; } }`,
+    `@custom-media --sm (min-width: 780px); @responsive { .bg-tomato { background: tomato; } .bg-coral { background: coral; } }`,
     [
+      `@custom-media --sm (min-width: 780px);`,
       `.bg-tomato { background: tomato; } .bg-coral { background: coral; }`,
-      `@media (--sm) { .bg-tomato\\@sm { background: tomato; } .bg-coral\\@sm { background: coral; } }`,
+      `@media (--sm){ .bg-tomato\\@sm { background: tomato; } .bg-coral\\@sm { background: coral; } }`,
     ].join(' ')
   );
 });
 
 it('supports multiple selectors per rule', async () => {
   await run(
-    `@responsive { .bg-tomato, .tomato-bg { background: tomato; } }`,
+    `@custom-media --sm (min-width: 780px); @responsive { .bg-tomato, .tomato-bg { background: tomato; } }`,
     [
+      `@custom-media --sm (min-width: 780px);`,
       `.bg-tomato, .tomato-bg { background: tomato; }`,
-      `@media (--sm) { .bg-tomato\\@sm, .tomato-bg\\@sm { background: tomato; } }`,
+      `@media (--sm){ .bg-tomato\\@sm, .tomato-bg\\@sm { background: tomato; } }`,
+    ].join(' ')
+  );
+});
+
+it('creates one media query per custom-media at-rule', async () => {
+  await run(
+    `@custom-media --sm (min-width: 780px); @custom-media --md (min-width: 900px); @custom-media --lg (min-width: 1200px); @responsive { .bg-tomato { background: tomato; } }`,
+    [
+      `@custom-media --sm (min-width: 780px);`,
+      `@custom-media --md (min-width: 900px);`,
+      `@custom-media --lg (min-width: 1200px);`,
+      `.bg-tomato { background: tomato; }`,
+      `@media (--sm){ .bg-tomato\\@sm { background: tomato; } }`,
+      `@media (--md){ .bg-tomato\\@md { background: tomato; } }`,
+      `@media (--lg){ .bg-tomato\\@lg { background: tomato; } }`,
     ].join(' ')
   );
 });
